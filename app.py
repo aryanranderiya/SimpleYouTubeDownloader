3eb754a0b35ce9004751513493db26f315aab1eb
AryanRanderiyaWorkstation <aryan.k.randeriya@gmail.com>
2024-04-15 19:24:53 +0530

    Fixed colon, Added Multithreading, Improved playlist formatting

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from pytube import Playlist
 from pathlib import Path
 import os
 import sys
+import threading
 
-downloads_path = str(Path.home() / "Desktop")
+# downloads_path = str(Path.home() / "Desktop")
+downloads_path = r"E:\AryanRanderiyaPC\PCHDDLibraries\Videos"
 filesize = ""
 
 
@@ -23,6 +25,10 @@ def download_video(link: str) -> None:
     print(f"Video {yt.title} Downloaded at {directory}!")
 
 
+def download_p_video(video, directory):
+    video.streams.first().download(output_path=directory)
+
+
 def download_playlist(link: str) -> None:
     p = Playlist(link)
     print(f'Downloading Playlist: {p.title}')
@@ -31,24 +37,36 @@ def download_playlist(link: str) -> None:
     directory: str = f"{downloads_path}/{folder_name}"
     os.makedirs(directory, exist_ok=True)
 
+    threads = []
+
     for index, video in enumerate(p.videos, start=1):
-        print(f'Downloading Video: "{video.title}" {index}/{p.length}')
-        video.register_on_progress_callback(on_progress)
-        video.streams.first().download(output_path=directory, )
+        vid_number: str = f"{index}/{p.length}  "
+        print(f'Downloading Video {vid_number} : "{video.title}"')
+
+        video.register_on_progress_callback(lambda stream, chunk, bytes_remaining, title=vid_number+video.title:
+                                            on_progress(stream, chunk, bytes_remaining, title))
+
+        thread = threading.Thread(
+            target=download_p_video, args=(video, directory))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
 
     print(f"Playlist {p.title} Downloaded at {directory}!")
 
 
-def on_progress(stream, chunk, bytes_remaining) -> None:
+def on_progress(stream, chunk, bytes_remaining, title) -> None:
     total_size = stream.filesize
     bytes_downloaded = total_size - bytes_remaining
     percentage = int(bytes_downloaded / total_size * 100)
-    print(f"""{percentage}% : {
+    print(f"""{title} : {percentage}% : {
           bytes_downloaded}/{total_size} Chunks Downloaded""", flush=True)
 
 
 def main() -> None:
-    link: str = input("Enter YouTube Link: (Video or Playlist)")
+    link: str = input("Enter YouTube Link: (Video or Playlist): ")
 
     if "playlist" in link:
         download_playlist(link)
